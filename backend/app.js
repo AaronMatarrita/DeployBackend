69dd7dd996d8f215fbc07f80b39c5e8c7103fd79
AaronMatarrita <aaron.matarrita.portuguez@est.una.ac.cr>
2025-05-05 00:39:21 -0600

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -32,6 +32,13 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(morgan("dev"));
 app.use(cookieParser());
+app.use((req, res, next) => {
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; font-src 'self';"
+  );
+  next();
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
